1bab257ff03ac51fb54f0475efe1754cb9febbc0
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2021-11-27 21:51:17 +0530

    added styled objects in comp3

--- src/components/PartnershipProgrammes/Components/PPComp3/PPComp3.jsx
+++ src/components/PartnershipProgrammes/Components/PPComp3/PPComp3.jsx
@@ -1,5 +1,6 @@
 import "../PPComp3/PPComp3.css"
-import img3 from "../images/img3.jpg"
+import img3back from "../images/img3back.png"
+import rec7 from "../images/rec-7.png"
 
 export default function PPComp3() {
     return (
@@ -7,6 +8,9 @@ export default function PPComp3() {
             <p className="ppcHeading ppc3Heading">Discovery Calls</p><br />
             <p className="ppcPara">Every month the leadership team encourages discovery calls that can be directly booked on our platform. Those who wish to grow their sales and business development skills can express their interest with us.Join our commercial partnership programme to earn regular commissions on referrals. Be a part of the Corporality vision.</p><br />
             <a className='ppc3Button' href="#">Know More</a>
+            <img className="ppc3Img3back" src={img3back} alt="" />
+            <img className="ppc3rec7-1" src={rec7} alt="" />
+            <img className="ppc3rec7-2" src={rec7} alt="" />
         </div>
     )
 }
