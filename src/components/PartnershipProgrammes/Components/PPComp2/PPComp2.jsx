0aa79cea55286d52c49a676bc3bc5e320239ec58
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2021-11-28 10:24:03 +0530

    added AOS in comp2

--- src/components/PartnershipProgrammes/Components/PPComp2/PPComp2.jsx
+++ src/components/PartnershipProgrammes/Components/PPComp2/PPComp2.jsx
@@ -3,22 +3,29 @@ import img2 from "../images/img2.png"
 import rec8 from "../images/rec-8.png"
 import wall from "../images/wall.png"
 import img2back from "../images/img2back.png"
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 export default function PPComp2() {
+    useEffect(() => {
+        AOS.init();
+        AOS.refresh();
+    }, []);
     return (
         <div>
             <div className="container-fluid ">
                 <div className="row ppc2RowContainer">
                     <div className="col d-flex">
                         <img className="ppc2Img2back" src={img2back} alt="" />
-                        <img className="ppc2Img2" src={img2} alt="" />
+                        <img data-aos="fade-right" data-aos-duration="1000" className="ppc2Img2" src={img2} alt="" />
                         <img className="ppc2Wall" src={wall} alt="" />
                         <img className="ppc2Rec8" src={rec8} alt="" />
                     </div>
                     <div className="col ppc2ColContainer">
-                        <p className="ppcHeading">Conversations with more than 100 entrepreneurs across the globe</p><br />
-                        <p>We have spoken to global leaders from manufacturing, to medtech, to hospitality and geospatial initiators.</p><br />
-                        <a className='ppc2Button' href="#">Check out Our set of Interviews</a>
+                        <p data-aos="fade-left" data-aos-duration="500"className="ppcHeading">Conversations with more than 100 entrepreneurs across the globe</p><br />
+                        <p data-aos="fade-left" data-aos-delay="300" data-aos-duration="500">We have spoken to global leaders from manufacturing, to medtech, to hospitality and geospatial initiators.</p><br />
+                        <a data-aos="fade-left" data-aos-delay="600" data-aos-duration="500"className='ppc2Button' href="#">Check out Our set of Interviews</a>
                     </div>
                 </div>
             </div>
