df488d2720b472c13d4de8ad4834f84f9806e119
Your Name <you@example.com>
2021-11-28 23:11:01 +0530

    updated cxo strategy

--- src/components/CxoStrategy/Components/Continously.js
+++ src/components/CxoStrategy/Components/Continously.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
 import Aos from 'aos';
 import 'aos/dist/aos.css';
-
+import "./CxoStrategy.css"
 
 export default function Continously() {
 
@@ -22,14 +22,14 @@ export default function Continously() {
           loop
           className="myVideo2"
         >
-          <source src="./img/continously/website2.mp4" type="video/mp4" />
+          <source src="./img/CxoStrategy/img/continously/website2.mp4" type="video/mp4" />
         </video>
 
         <div class="container cxo-video-box-container">
-          <h1 >
+          <h1 data-aos="fade-left" data-aos-duration="1500" data-aos-delay="800">
             Continuously working towards an organisation’s relationship goals
           </h1>
-          <p>
+          <p data-aos="fade-left" data-aos-duration="1500" data-aos-delay="1200">
             A long-term sustainable customer plan is better than a short-term
             non-sustainable marketing strategy that is expensive. Choose the
             best defence against the churn as you hire a CXO that can navigate
