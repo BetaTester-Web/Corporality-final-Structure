b5e1e812f3fc0ece7af93c6fb71eb979f54fca2e
Kk015 <kunalmathur0803@gmail.com>
2021-11-29 11:19:08 +0530

    Enter

--- src/components/BrandPositioning/BrandPos-Components/BP-Component-2/BP-Comp-2.js
+++ src/components/BrandPositioning/BrandPos-Components/BP-Component-2/BP-Comp-2.js
@@ -3,37 +3,20 @@ import "./BP-Comp-2.css";
 
 function BPComponent2() {
   return (
-    <div className="main">
-      <div>
+    <div className="bp-cp2-main">
+      <div className="bp-cp2-img1-d">
         <img
-          src=".\img\BrandPositioning\BP-Comp2.png"
-          alt="Brand-Positioning"
-          className="BP-CP-2-img"
-        ></img>
-      </div>
-      <div className="bp-c2-s1-b2">
-        <img
-          src="..\img\BrandPositioning\Vector-19.png"
-          alt=""
-          className="bp-cp-2-small-triangle"
+          src="..\img\BrandPositioning\BP-Comp2.png"
+          alt="BP-CP-2"
+          className="bp-cp2-img1"
         />
       </div>
-
-      <div className="bp-c2-section-1 ">
-        <div className="bp-c2-s1-b3"> </div>
-
-        <div className="bp-c2-s1-b1">
-          <h1 className="bp-c2-s1-h1">Brand Positioning </h1>
-        </div>
-
-        <p className="bp-c2-s1-p">
-          Position the brand in the minds of the customers in a way that they
-          never forget you. Set your business apart from the competition with a
-          potent combination of credibility and value. Create a unique appeal
-          that enraptures the customer. It is not enough to be different anymore
-          - it is time to carve yourselves into the heart and mind of the
-          customer.
-        </p>
+      <div className="bp-cp2-img2-d">
+        <img
+          src="..\img\BrandPositioning\Rectangle-261.png"
+          alt="BP-CP-2"
+          className="bp-cp2-img2"
+        />
       </div>
     </div>
   );
