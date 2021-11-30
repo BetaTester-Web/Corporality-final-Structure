4e0170039b6901efb0c129e91ebf7e67c3e41dcb
Kk015 <kunalmathur0803@gmail.com>
2021-11-30 14:49:08 +0530

    Improved component 4

--- src/components/BrandPositioning/BrandPos-Components/BP-Component-4/BP-Component-4.js
+++ src/components/BrandPositioning/BrandPos-Components/BP-Component-4/BP-Component-4.js
@@ -10,31 +10,29 @@ import "./BP-Component4.css";
 function BPComponent4() {
   return (
     <div className="bp-cp-4-main">
+      <div className="BP-c3-d">
+        <h1 className="BP-c3-h">
+          Articulating yourself - Creating magic from the basics{" "}
+        </h1>
+        <p className="BP-c3-p">
+          An effective brand positioning and marketing strategy is all about
+          taking the most effective steps towards becoming clear, differentiated
+          and meaningful. Corporality Global evaluates your current branding
+          orientation in combination with market analytics to help you
+          articulate yourself. You need a roadmap that clearly presents who you
+          are - what you do - why you matter, making positioning, not just
+          beneficial but sustainable as well. We then tie the positioning to an
+          already existing idea of your company with the target audience and
+          shift that truth to your audience’s advantage.
+        </p>
+      </div>
+
       <div className="BP-CP-4-img-d">
         <img
           src="..\img\BrandPositioning\BP-Comp3.png"
           alt="BP-CP-4"
           className="BP-CP-4-img img-fluid"
         />
-      </div>
-      <div className="bp-cd4-textdiv translate-middle-y">
-        <div className="BP-c3-d">
-          <h1 className="BP-c3-h">
-            Articulating yourself - Creating magic from the basics{" "}
-          </h1>
-          <p className="BP-c3-p">
-            An effective brand positioning and marketing strategy is all about
-            taking the most effective steps towards becoming clear,
-            differentiated and meaningful. Corporality Global evaluates your
-            current branding orientation in combination with market analytics to
-            help you articulate yourself. You need a roadmap that clearly
-            presents who you are - what you do - why you matter, making
-            positioning, not just beneficial but sustainable as well. We then
-            tie the positioning to an already existing idea of your company with
-            the target audience and shift that truth to your audience’s
-            advantage.
-          </p>
-        </div>
       </div>
     </div>
   );
