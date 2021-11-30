8fc40f1dbc450a2cf9ccbcc1f18ff0f5a12f25d6
Kk015 <kunalmathur0803@gmail.com>
2021-11-30 20:58:53 +0530

    added } in BrandPosition.js

--- src/components/BrandPositioning/BrandPositioning.js
+++ src/components/BrandPositioning/BrandPositioning.js
@@ -1,131 +1,248 @@
-import React, { useEffect } from 'react';
-import Aos from 'aos';
-import 'aos/dist/aos.css';
-import "./BrandPositioning.css"
+import React, { useEffect } from "react";
+import Aos from "aos";
+import "aos/dist/aos.css";
+import "./BrandPositioning.css";
 import BPComponent2 from "./BrandPos-Components/BP-Component-2/BP-Comp-2";
 import BPComponent4 from "./BrandPos-Components/BP-Component-4/BP-Component-4";
 import BPComponent5 from "./BrandPos-Components/BP-Component-5/BP-Comp-5";
 
 export default function Component1() {
-
-    useEffect(() => {
-        Aos.init();
-        Aos.init({ disable: 'mobile' })
-    })
-    return (
-        <>
-            <div className="vw-100 fullSection position-relative">
-                <div className="snakes position-absolute end-0 top-0">
-                    <img src="/img/BrandPositioning/Group 1252.png" alt="" className="img-fluid" />
-                </div>
-                <div className="container py-0 landingPage position-relative">
-
-                    <div className="mediumD1 position-absolute end-50 top-0">
-                        <img src="/img/BrandPositioning/Rectangle 67.png" alt="" className="img-fluid" />
-                    </div>
-                    <div className="mediumD2 position-absolute end-50 bottom-0">
-                        <img src="/img/BrandPositioning/Rectangle 68.png" alt="" className="img-fluid" />
-                    </div>
-                    <div className="vw-100 position-relative flexing d-sm-flex justify-content-center">
-                        
-                        <div className="art position-relative position-absolute top-50 end-0 translate-middle-y">
-                            <div data-aos="fade-left"
-                                data-aos-anchor="#example-anchor"
-                                data-aos-offset="500"
-                                data-aos-duration="3000" className="imprvdCompBlock position-absolute top-0 end-0 position-relative">
-                                <img src="/img/BrandPositioning/Group 1243.png" alt="" className="imprvdCompImg position-absolute start-0 top-0" />
-                                <p className="imprvdCompText my-0 position-absolute bottom-0 end-0 text-wrap">
-                                    Improved company image
-                                </p>
-                            </div>
-                            <div data-aos="fade-left"
-                                data-aos-anchor="#example-anchor"
-                                data-aos-offset="500"
-                                data-aos-duration="1500" className="custLoyaltyBlock position-absolute bottom-50 start-0 position-relative">
-                                <img src="/img/BrandPositioning/Group 1242.png" alt="" className="custLoyaltyImg position-absolute start-0 top-0" />
-                                <p className="custLoyaltyText my-0 position-absolute bottom-0 end-0 text-wrap">
-                                    Customer loyalty
-                                </p>
-                            </div>
-                            <div data-aos="fade-left"
-                                data-aos-anchor="#example-anchor"
-                                data-aos-offset="500"
-                                data-aos-duration="2000" className="relatableIDBlock position-absolute bottom-50 end-0 position-relative">
-                                <img src="/img/BrandPositioning/Group 1244.png" alt="" className="relatableIDImg position-absolute start-0 top-0" />
-                                <p className="relatableIDText my-0 position-absolute bottom-0 end-0 text-wrap">
-                                    Relatable identity
-                                </p>
-                            </div>
-                            <div data-aos="fade-left"
-                                data-aos-anchor="#example-anchor"
-                                data-aos-offset="500"
-                                data-aos-duration="1000" className="omniBlock position-absolute top-50 start-50 position-relative">
-                                <img src="/img/BrandPositioning/Group 1248.png" alt="" className="omniImg position-absolute start-0 top-0" />
-                                <p className="omniText my-0 position-absolute bottom-0 end-0 text-wrap">
-                                    Omni effect vs experience
-                                </p>
-                            </div>
-                            <div className="phnbtn position-absolute end-0 bottom-0">
-                                <img src="/img/BrandPositioning/Group 709.png" alt="" className="img-fluid" />
-                            </div>
-                            <div className="littleD1 position-absolute end-0 top-50">
-                                <img src="/img/BrandPositioning/Rectangle 69.png" alt="" className="img-fluid" />
-                            </div>
-                            <div className="littleD2 position-absolute start-0 bottom-50">
-                                <img src="/img/BrandPositioning/Rectangle 69.png" alt="" className="img-fluid" />
-                            </div>
-                            <div data-aos="fade-up"
-                                data-aos-anchor="#example-anchor"
-                                data-aos-offset="500"
-                                data-aos-duration="2000" className='textD position-absolute start-0 bottom-0'>
-                                <img src="/img/BrandPositioning/Group 1250.png" alt="" className="img-fluid" />
-                            </div>
-                        </div>
-                        <div className="brandPara mx-auto text-wrap position-absolute start-0 top-50 translate-middle-y">
-                            <p data-aos="fade-up"
-                                data-aos-easing="linear"
-                                data-aos-duration="1000" className="paraHead my-2 fw-bold">
-                                EVERY BRAND HAS A STORY AND EVERY STORY IS UNIQUE
-                            </p>
-                            <p data-aos="fade-down"
-                                data-aos-easing="linear"
-                                data-aos-duration="1500" className="paraText my-2">
-                                Big brands have one thing in common - they have a solid brand positioning strategy. Businesses must invest in a strong brand marketing strategy to thrive in a highly competitive market. Why? The proof lies in the numbers. Companies continuously investing in brand positioning strategies can see an average increase in revenue between 10 and 20%.
-                            </p>
-                        </div>
-                    </div>
-                </div>
-                <div className="social position-absolute end-0 top-50 translate-middle-y">
-                    <div class="container">
-                        <div class="row row-cols-1">
-                            <div class="col my-1 socialDiv">
-                                <a href="https://www.facebook.com/CorporalityG" target="_blank"><img src="/img/BrandPositioning/Group 618.png" alt="" className="img-fluid" /></a>
-                            </div>
-                            <div class="col my-1 socialDiv">
-                                <a href="https://twitter.com/corporalityg" target="_blank"><img src="/img/BrandPositioning/Group 617.png" alt="" className="img-fluid" /></a>
-                            </div>
-                            <div class="col my-1 socialDiv">
-                                <a href="https://www.instagram.com/corporalityg" target="_blank"><img src="/img/BrandPositioning/Group 616.png" alt="" className="img-fluid" /></a>
-                            </div>
-                            <div class="col my-1 socialDiv">
-                                <a href="https://www.linkedin.com/company/corporality" target="_blank"><img src="/img/BrandPositioning/Group 615.png" alt="" className="img-fluid" /></a>
-                            </div>
-                            <div class="col my-1 socialDiv">
-                                <a href="https://in.pinterest.com/CorporalityG" target="_blank"><img src="/img/BrandPositioning/Group 614.png" alt="" className="img-fluid" /></a>
-                            </div>
-                            <div class="col my-1 socialDiv">
-                                <a href="https://www.youtube.com/channel/UC4EISt8kHI4zzpmbIBMIBbg" target="_blank"><img src="/img/BrandPositioning/Group 613.png" alt="" className="img-fluid" /></a>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    Aos.init();
+    Aos.init({ disable: "mobile" });
+  });
+  return (
+    <>
+      <div className="vw-100 fullSection position-relative">
+        <div className="snakes position-absolute end-0 top-0">
+          <img
+            src="/img/BrandPositioning/Group 1252.png"
+            alt=""
+            className="img-fluid"
+          />
+        </div>
+        <div className="container py-0 landingPage position-relative">
+          <div className="mediumD1 position-absolute end-50 top-0">
+            <img
+              src="/img/BrandPositioning/Rectangle 67.png"
+              alt=""
+              className="img-fluid"
+            />
+          </div>
+          <div className="mediumD2 position-absolute end-50 bottom-0">
+            <img
+              src="/img/BrandPositioning/Rectangle 68.png"
+              alt=""
+              className="img-fluid"
+            />
+          </div>
+          <div className="vw-100 position-relative flexing d-sm-flex justify-content-center">
+            <div className="art position-relative position-absolute top-50 end-0 translate-middle-y">
+              <div
+                data-aos="fade-left"
+                data-aos-anchor="#example-anchor"
+                data-aos-offset="500"
+                data-aos-duration="3000"
+                className="imprvdCompBlock position-absolute top-0 end-0 position-relative"
+              >
+                <img
+                  src="/img/BrandPositioning/Group 1243.png"
+                  alt=""
+                  className="imprvdCompImg position-absolute start-0 top-0"
+                />
+                <p className="imprvdCompText my-0 position-absolute bottom-0 end-0 text-wrap">
+                  Improved company image
+                </p>
+              </div>
+              <div
+                data-aos="fade-left"
+                data-aos-anchor="#example-anchor"
+                data-aos-offset="500"
+                data-aos-duration="1500"
+                className="custLoyaltyBlock position-absolute bottom-50 start-0 position-relative"
+              >
+                <img
+                  src="/img/BrandPositioning/Group 1242.png"
+                  alt=""
+                  className="custLoyaltyImg position-absolute start-0 top-0"
+                />
+                <p className="custLoyaltyText my-0 position-absolute bottom-0 end-0 text-wrap">
+                  Customer loyalty
+                </p>
+              </div>
+              <div
+                data-aos="fade-left"
+                data-aos-anchor="#example-anchor"
+                data-aos-offset="500"
+                data-aos-duration="2000"
+                className="relatableIDBlock position-absolute bottom-50 end-0 position-relative"
+              >
+                <img
+                  src="/img/BrandPositioning/Group 1244.png"
+                  alt=""
+                  className="relatableIDImg position-absolute start-0 top-0"
+                />
+                <p className="relatableIDText my-0 position-absolute bottom-0 end-0 text-wrap">
+                  Relatable identity
+                </p>
+              </div>
+              <div
+                data-aos="fade-left"
+                data-aos-anchor="#example-anchor"
+                data-aos-offset="500"
+                data-aos-duration="1000"
+                className="omniBlock position-absolute top-50 start-50 position-relative"
+              >
+                <img
+                  src="/img/BrandPositioning/Group 1248.png"
+                  alt=""
+                  className="omniImg position-absolute start-0 top-0"
+                />
+                <p className="omniText my-0 position-absolute bottom-0 end-0 text-wrap">
+                  Omni effect vs experience
+                </p>
+              </div>
+              <div className="phnbtn position-absolute end-0 bottom-0">
+                <img
+                  src="/img/BrandPositioning/Group 709.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
+              <div className="littleD1 position-absolute end-0 top-50">
+                <img
+                  src="/img/BrandPositioning/Rectangle 69.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
+              <div className="littleD2 position-absolute start-0 bottom-50">
+                <img
+                  src="/img/BrandPositioning/Rectangle 69.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
+              <div
+                data-aos="fade-up"
+                data-aos-anchor="#example-anchor"
+                data-aos-offset="500"
+                data-aos-duration="2000"
+                className="textD position-absolute start-0 bottom-0"
+              >
+                <img
+                  src="/img/BrandPositioning/Group 1250.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </div>
             </div>
-            <div className="my-1 vw-100">
-                <img src="/img/BrandPositioning/Frame 204.png" alt="" className="img-fluid"/>
+            <div className="brandPara mx-auto text-wrap position-absolute start-0 top-50 translate-middle-y">
+              <p
+                data-aos="fade-up"
+                data-aos-easing="linear"
+                data-aos-duration="1000"
+                className="paraHead my-2 fw-bold"
+              >
+                EVERY BRAND HAS A STORY AND EVERY STORY IS UNIQUE
+              </p>
+              <p
+                data-aos="fade-down"
+                data-aos-easing="linear"
+                data-aos-duration="1500"
+                className="paraText my-2"
+              >
+                Big brands have one thing in common - they have a solid brand
+                positioning strategy. Businesses must invest in a strong brand
+                marketing strategy to thrive in a highly competitive market.
+                Why? The proof lies in the numbers. Companies continuously
+                investing in brand positioning strategies can see an average
+                increase in revenue between 10 and 20%.
+              </p>
             </div>
-         <BPComponent2 />
+          </div>
+        </div>
+        <div className="social position-absolute end-0 top-50 translate-middle-y">
+          <div class="container">
+            <div class="row row-cols-1">
+              <div class="col my-1 socialDiv">
+                <a href="https://www.facebook.com/CorporalityG" target="_blank">
+                  <img
+                    src="/img/BrandPositioning/Group 618.png"
+                    alt=""
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+              <div class="col my-1 socialDiv">
+                <a href="https://twitter.com/corporalityg" target="_blank">
+                  <img
+                    src="/img/BrandPositioning/Group 617.png"
+                    alt=""
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+              <div class="col my-1 socialDiv">
+                <a
+                  href="https://www.instagram.com/corporalityg"
+                  target="_blank"
+                >
+                  <img
+                    src="/img/BrandPositioning/Group 616.png"
+                    alt=""
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+              <div class="col my-1 socialDiv">
+                <a
+                  href="https://www.linkedin.com/company/corporality"
+                  target="_blank"
+                >
+                  <img
+                    src="/img/BrandPositioning/Group 615.png"
+                    alt=""
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+              <div class="col my-1 socialDiv">
+                <a href="https://in.pinterest.com/CorporalityG" target="_blank">
+                  <img
+                    src="/img/BrandPositioning/Group 614.png"
+                    alt=""
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+              <div class="col my-1 socialDiv">
+                <a
+                  href="https://www.youtube.com/channel/UC4EISt8kHI4zzpmbIBMIBbg"
+                  target="_blank"
+                >
+                  <img
+                    src="/img/BrandPositioning/Group 613.png"
+                    alt=""
+                    className="img-fluid"
+                  />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="my-1 vw-100">
+        <img
+          src="/img/BrandPositioning/Frame 204.png"
+          alt=""
+          className="img-fluid"
+        />
+      </div>
+      <BPComponent2 />
 
       <BPComponent4 />
       <BPComponent5 />
-        </>
-    )
+    </>
+  );
+}
