8048b91dd740a142154bbdcc99a0ffa1a86d221d
Dikshansh Kumar Lodhi <dikshansh1712@gmail.com>
2021-11-30 19:38:28 +0530

    SAC: Added Comp3

--- src/components/StrategyAndConsulting/StrategyAndConsulting.js
+++ src/components/StrategyAndConsulting/StrategyAndConsulting.js
@@ -1,5 +1,6 @@
 import SACComp1 from "./Components/SACComp1"
 import SACComp2 from "./Components/SACComp2"
+import SACComp3 from "./Components/SACComp3"
 import SACComp4 from "./Components/SACComp4"
 import SACComp5 from "./Components/SACComp5"
 import SACComp6 from "./Components/SACComp6"
@@ -9,6 +10,7 @@ export default function StrategyAndConsulting() {
         <div>
             <SACComp1 />
             <SACComp2 />
+            <SACComp3 />
             <SACComp4 />
             <SACComp5 />
             <SACComp6 />
