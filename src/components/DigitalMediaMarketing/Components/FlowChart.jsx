dd1dcb4004ceaf9657718573375265424c4a1957
Your Name <you@example.com>
2021-12-02 22:33:55 +0530

    DMM update

--- src/components/DigitalMediaMarketing/Components/FlowChart.jsx
+++ src/components/DigitalMediaMarketing/Components/FlowChart.jsx
@@ -5,12 +5,201 @@ export default function FlowChart() {
 	return (
 		<>
 			<div className="DDMFlowChart">
-				
+
 				<div className="DDMFlowChartText">
 					<h1>From pitch to picture perfect scenario</h1>
 					<p>The process of starting a digital media marketing with Corporality Global -An innovative lead generation and marketing company in Sydney, and approaching the right strategy can be highly intended to your true audience. We’ve created this service Edition to accompany founders in this journey, from getting their vision and approaching potential buyers' persona naturally.</p>
 				</div>
-				
+				<div class="DDMparent">
+
+					{/* Row 1 */}
+					<div class="DDMdiv1">
+						<div className="DDMdiv1Text">
+							Planning Phase
+						</div>
+						<div className="DDMdiv1Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Joiner.png" alt="" />
+						</div>
+					</div>
+					<div class="DDMdiv2">
+						<div className="DDMdiv2Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/MarketingAudit.png" alt="" />
+						</div>
+						<div className="DDMdiv2Text">
+							Marketing Audit
+						</div>
+					</div>
+					<div class="DDMdiv3">
+						<div className="DDMdiv3Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Inbound.png" alt="" />
+						</div>
+						<div className="DDMdiv3Text">
+							Inbound Marketing Concept to proposed plan
+						</div>
+					</div>
+					<div class="DDMdiv4">
+						<div className="DDMdiv4Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/MasterMind.png" alt="" />
+						</div>
+						<div className="DDMdiv4Text">
+							Master minding workshop
+						</div>
+					</div>
+					<div class="DDMdiv5">
+						<div className="DDMdiv5Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Accountability.png" alt="" />
+						</div>
+						<div className="DDMdiv5Text">
+							Presentation and Accountability
+						</div>
+					</div>
+					<div class="DDMdiv6">
+						<div className="DDMdiv6Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Talent.png" alt="" />
+						</div>
+						<div className="DDMdiv6Text">
+							Talent Alignment
+						</div>
+					</div>
+					<div class="DDMdiv7">
+						<div className="DDMdiv7Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Strategy.png" alt="" />
+						</div>
+						<div className="DDMdiv7Text">
+							Strategy Preparation
+						</div>
+					</div>
+
+					{/* row 2 */}
+					<div class="DDMdiv8">
+						<div className="DDMdiv8Text">
+							Prep Phase
+						</div>
+						<div className="DDMdiv8Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Joiner.png" alt="" />
+						</div>
+					</div>
+					<div class="DDMdiv9">
+						<div className="DDMdiv9Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Targeting.png" alt="" />
+						</div>
+						<div className="DDMdiv9Text">
+							Channel Targeting
+						</div>
+					</div>
+					<div class="DDMdiv10">
+						<div className="DDMdiv10Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/SocialMedia.png" alt="" />
+						</div>
+						<div className="DDMdiv10Text">
+							Social Media Marketing Strategy
+						</div>
+					</div>
+					<div class="DDMdiv11">
+						<div className="DDMdiv11Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Content.png" alt="" />
+						</div>
+						<div className="DDMdiv11Text">
+							Content Strategy
+						</div>
+					</div>
+					<div class="DDMdiv12">
+						<div className="DDMdiv12Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/SEO.png" alt="" />
+						</div>
+						<div className="DDMdiv12Text">
+							SEO Strategy
+						</div>
+					</div>
+					<div class="DDMdiv13">
+						<div className="DDMdiv13Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Searches.png" alt="" />
+						</div>
+						<div className="DDMdiv13Text">
+							Social Searches Strategy
+						</div>
+					</div>
+					<div class="DDMdiv14">
+						<div className="DDMdiv14Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/EmailApproach.png" alt="" />
+						</div>
+						<div className="DDMdiv14Text">
+							Email Marketing Approach
+						</div>
+					</div>
+
+					{/* row 3 */}
+					<div class="DDMdiv15">
+						<div className="DDMdiv15Text">
+							Execution Phase
+						</div>
+						<div className="DDMdiv15Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/MarketingAudit.png" alt="" />
+						</div>
+
+					</div>
+					<div class="DDMdiv16">
+						<div className="DDMdiv16Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Implementation.png" alt="" />
+						</div>
+						<div className="DDMdiv16Text">
+							On-page SEO Implementation
+						</div>
+					</div>
+					<div class="DDMdiv17">
+						<div className="DDMdiv17Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Action.png" alt="" />
+						</div>
+						<div className="DDMdiv17Text">
+							Off-page SEO Action defined
+						</div>
+					</div>
+					<div class="DDMdiv18">
+						<div className="DDMdiv18Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Action.png" alt="" />
+						</div>
+						<div className="DDMdiv18Text">
+							Search Engine Marketing
+						</div>
+					</div>
+					<div class="DDMdiv19">
+						<div className="DDMdiv19Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Preparation.png" alt="" />
+						</div>
+						<div className="DDMdiv19Text">
+							Content Preparation
+						</div>
+					</div>
+					<div class="DDMdiv20">
+						<div className="DDMdiv20Img">
+							<img src="./img/DigitalMediaMarketing/img/Flowchart/Testing.png" alt="" />
+						</div>
+						<div className="DDMdiv20Text">
+							A/B Testing on Inbound Strategy
+						</div>
+					</div>
+					<div class="DDMdiv21">
+
+					</div>
+
+					{/* row 4 */}
+					<div class="DDMdiv22">
+						Demo </div>
+					<div class="DDMdiv23">
+						Demo </div>
+					<div class="DDMdiv24">
+						Demo </div>
+					<div class="DDMdiv25">
+						Demo </div>
+					<div class="DDMdiv26">
+						Demo </div>
+					<div class="DDMdiv27">
+						Demo </div>
+					<div class="DDMdiv28">
+						Demo </div>
+				</div>
+
+
 			</div>
 		</>
 	)
