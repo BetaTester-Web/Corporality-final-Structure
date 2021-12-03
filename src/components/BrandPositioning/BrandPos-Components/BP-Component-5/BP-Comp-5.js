906211c5d3b87f396dc27ef3d7cde311a53fd05f
Kunal <kunalmathur0803@gmail.com>
2021-12-03 21:45:50 +0530

    installed bootstrap npm and recreated comp 5

--- src/components/BrandPositioning/BrandPos-Components/BP-Component-5/BP-Comp-5.js
+++ src/components/BrandPositioning/BrandPos-Components/BP-Component-5/BP-Comp-5.js
@@ -1,23 +1,7 @@
-/* eslint-disable no-lone-blocks */
-/* eslint-disable jsx-a11y/no-redundant-roles */
+/* eslint-disable jsx-a11y/alt-text */
 import React from "react";
 import "./BP-Comp-5.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
-
-{
-  <link
-    href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
-    rel="stylesheet"
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-    crossorigin="anonymous"
-  ></link>;
-  <script
-    src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
-    crossorigin="anonymous"
-  ></script>;
-}
+import { Accordion } from "react-bootstrap";
 
 function BPComponent5() {
   return (
@@ -25,10 +9,9 @@ function BPComponent5() {
       <div className="BP-CP5-heading">
         <h1 className="BP-CP5-h1">Indomitable brand positioning strategies</h1>
       </div>
-      <div class="container">
-        <div class="row">
-          <div class="col-2">
-            {" "}
+      <div className="container">
+        <div className="row">
+          <div className="col-1 ">
             <img
               src="..\img\BrandPositioning\cp-5-2.png"
               alt=""
@@ -40,190 +23,130 @@ function BPComponent5() {
               className="bp-cp-5-img-1 img-fluid "
             />
           </div>
-          <div class="col-10">
-            {" "}
-            <div className="bp-cd5-list-maindiv">
-              <div class="accordion" id="accordionPanelsStayOpenExample">
-                <div class="accordion-item bp-cp5-dd-head">
-                  <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                    <button
-                      class="accordion-button bp-cp5-dd-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#panelsStayOpen-collapseOne"
-                      aria-expanded="false"
-                      aria-controls="panelsStayOpen-collapseOne"
-                    >
-                      <img
-                        src="..\img\BrandPositioning\faviconTM1.png"
-                        alt=""
-                        className="bp-cp5-favicon-img"
-                      />
-                      Defining the true differentiators
-                    </button>
-                  </h2>
-                  <div
-                    id="panelsStayOpen-collapseOne"
-                    class="accordion-collapse collapse show bp-cp5-list-p"
-                    aria-labelledby="panelsStayOpen-headingOne"
-                  >
+          <div className="col-11">
+            <Accordion>
+              {" "}
+              <Accordion.Item eventKey="0">
+                <Accordion.Header>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-3">
+                        <img src="../img/BrandPositioning/faviconTM1.png" />
+                      </div>
+                      <div class="col-9">Defining the true differentiators</div>
+                    </div>
+                  </div>
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div class="container">
                     <div class="row">
                       <div class="col-2">
-                        <img
-                          src="..\img\BrandPositioning\Rectangle-1.png"
-                          alt=""
-                          className="img-fluid"
-                        />
+                        <img src="../img/BrandPositioning/Rectangle-1.png" />
                       </div>
                       <div class="col-10">
-                        <div class="accordion-body">
-                          Marketing and communication must be disposed of in a
-                          way that will stir consumer interest so that you get
-                          noticed by all. Therefore, we create a preference that
-                          will differentiate you completely from other market
-                          players that essentially transports the customer to a
-                          premium experience.
-                        </div>
+                        Marketing and communication must be disposed of in a way
+                        that will stir consumer interest so that you get noticed
+                        by all. Therefore, we create a preference that will
+                        differentiate you completely from other market players
+                        that essentially transports the customer to a premium
+                        experience.
                       </div>
                     </div>
                   </div>
-                </div>
-                <div class="accordion-item bp-cp5-dd-head">
-                  <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                    <button
-                      class="accordion-button collapsed bp-cp5-dd-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#panelsStayOpen-collapseTwo"
-                      aria-expanded="false"
-                      aria-controls="panelsStayOpen-collapseTwo"
-                    >
-                      <img
-                        src="..\img\BrandPositioning\faviconTM2.png"
-                        alt=""
-                        className="bp-cp5-favicon-img"
-                      />
-                      Maintaining relevance
-                    </button>
-                  </h2>
-                  <div
-                    id="panelsStayOpen-collapseTwo"
-                    class="accordion-collapse collapse bp-cp5-list-p"
-                    aria-labelledby="panelsStayOpen-headingTwo"
-                  >
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="1">
+                <Accordion.Header>
+                  <div class="container">
                     <div class="row">
-                      <div class="col-2">
-                        <img
-                          src="..\img\BrandPositioning\Rectangle-2.png"
-                          alt=""
-                          className="img-fluid"
-                        />
-                      </div>
-                      <div class="col-10">
-                        <div class="accordion-body">
-                          While true differentiators are important, compelling
-                          the target audience is vital to the success of a
-                          brand. At Corporality Global - the top digital
-                          marketing management firm in Sydney, we locate all
-                          pain points, including unfulfilled desires and focus
-                          on placing your brand where the audience needs it the
-                          most. You will be up for grabs in no time.
+                      <div class="row">
+                        <div class="col-3">
+                          <img src="../img/BrandPositioning/faviconTM2.png" />
                         </div>
+                        <div class="col-9">Maintaining relevance</div>
                       </div>
                     </div>
                   </div>
-                </div>
-                <div class="accordion-item bp-cp5-dd-head">
-                  <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                    <button
-                      class="accordion-button collapsed bp-cp5-dd-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#panelsStayOpen-collapseThree"
-                      aria-expanded="false"
-                      aria-controls="panelsStayOpen-collapseThree"
-                    >
-                      {" "}
-                      <img
-                        src="..\img\BrandPositioning\faviconTM3.png"
-                        alt=""
-                        className="bp-cp5-favicon-img"
-                      />
-                      Address and engage
-                    </button>
-                  </h2>
-                  <div
-                    id="panelsStayOpen-collapseThree"
-                    class="accordion-collapse collapse bp-cp5-list-p"
-                    aria-labelledby="panelsStayOpen-headingThree"
-                  >
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div class="container">
                     <div class="row">
                       <div class="col-2">
-                        <img
-                          src="..\img\BrandPositioning\Rectangle-3.png"
-                          alt=""
-                          className="img-fluid"
-                        />
+                        <img src="../img/BrandPositioning/Rectangle-2.png" />
                       </div>
                       <div class="col-10">
-                        <div class="accordion-body">
-                          Coherence is essential to engage with your audiences
-                          and connect with them in meaningful ways. Our Sydney
-                          based professional digital marketing create a unique
-                          value that appeals to the people, and in the end, you
-                          are celebrated by them.
-                        </div>
+                        While true differentiators are important, compelling the
+                        target audience is vital to the success of a brand. At
+                        Corporality Global - the top digital marketing
+                        management firm in Sydney, we locate all pain points,
+                        including unfulfilled desires and focus on placing your
+                        brand where the audience needs it the most. You will be
+                        up for grabs in no time.
                       </div>
                     </div>
                   </div>
-                </div>
-                <div class="accordion-item bp-cp5-dd-head">
-                  <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                    <button
-                      class="accordion-button collapsed bp-cp5-dd-button"
-                      type="button"
-                      data-bs-toggle="collapse"
-                      data-bs-target="#panelsStayOpen-collapseFour"
-                      aria-expanded="false"
-                      aria-controls="panelsStayOpen-collapseFour"
-                    >
-                      <img
-                        src="..\img\BrandPositioning\faviconTM4.png"
-                        alt=""
-                        className="bp-cp5-favicon-img"
-                      />
-                      Earn a reputation
-                    </button>
-                  </h2>
-                  <div
-                    id="panelsStayOpen-collapseFour"
-                    class="accordion-collapse collapse bp-cp5-list-p "
-                    aria-labelledby="panelsStayOpen-headingFour"
-                  >
-                    {" "}
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="2">
+                <Accordion.Header>
+                  <div class="container">
                     <div class="row">
-                      <div class="col-2">
-                        <img
-                          src="..\img\BrandPositioning\Rectangle-4.png"
-                          alt=""
-                          className="img-fluid"
-                        />
-                      </div>
-                      <div class="col-10">
-                        {" "}
-                        <div class="accordion-body">
-                          Fulfilling expectations over and over again gives you
-                          a reputation of being a committed performer. We add
-                          intricate innovations to your brand positioning
-                          strategy that will create a winner’s attitude among
-                          users.
+                      <div class="row">
+                        <div class="col-3">
+                          <img src="../img/BrandPositioning/faviconTM3.png" />
                         </div>
+                        <div class="col-9">Address and engage</div>
                       </div>
                     </div>
                   </div>
-                </div>
-              </div>
-            </div>
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-2">
+                        <img src="../img/BrandPositioning/Rectangle-3.png" />
+                      </div>
+                      <div class="col-10">
+                        Coherence is essential to engage with your audiences and
+                        connect with them in meaningful ways. Our Sydney based
+                        professional digital marketing create a unique value
+                        that appeals to the people, and in the end, you are
+                        celebrated by them.
+                      </div>
+                    </div>
+                  </div>
+                </Accordion.Body>
+              </Accordion.Item>
+              <Accordion.Item eventKey="3">
+                <Accordion.Header>
+                  <div class="container">
+                    <div class="row">
+                      <div class="row">
+                        <div class="col-3">
+                          <img src="../img/BrandPositioning/faviconTM4.png" />
+                        </div>
+                        <div class="col-9"> Earn a reputation</div>
+                      </div>
+                    </div>
+                  </div>
+                </Accordion.Header>
+                <Accordion.Body>
+                  <div class="container">
+                    <div class="row">
+                      <div class="col-2">
+                        <img src="../img/BrandPositioning/Rectangle-4.png" />
+                      </div>
+                      <div class="col-10">
+                        Fulfilling expectations over and over again gives you a
+                        reputation of being a committed performer. We add
+                        intricate innovations to your brand positioning strategy
+                        that will create a winner’s attitude among users.
+                      </div>
+                    </div>
+                  </div>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
           </div>
         </div>
       </div>
