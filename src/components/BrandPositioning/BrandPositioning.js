e7ebbe9749289f043600ee924c4842461da59bfb
AKASH SINGH <furiousakash111@gmail.com>
2021-12-05 23:55:35 +0530

    animation on call button in bp component 1

--- src/components/BrandPositioning/BrandPositioning.js
+++ src/components/BrandPositioning/BrandPositioning.js
@@ -104,11 +104,11 @@ export default function Component1() {
                 </p>
               </div>
               <div className="phnbtn position-absolute end-0 bottom-0">
-                <img
-                  src="/img/BrandPositioning/Group-709.png"
-                  alt=""
-                  className="img-fluid"
-                />
+                  <img
+                    src="/img/BrandPositioning/Group-709.png"
+                    alt=""
+                    className="img-fluid"
+                  />
               </div>
               <div className="littleD1 position-absolute end-0 top-50">
                 <img
@@ -163,87 +163,87 @@ export default function Component1() {
             </div>
           </div>
         </div>
-        <div className="social position-absolute end-0 top-50 translate-middle-y container"> 
-            <div class="row row-cols-1 mx-0 bpcp1SocialRow">
-              <div class="col my-1 socialDiv">
-                <a
-                  href="https://www.facebook.com/CorporalityG"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="/img/BrandPositioning/Group-618.png"
-                    alt=""
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-              <div class="col my-1 socialDiv">
-                <a
-                  href="https://twitter.com/corporalityg"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="/img/BrandPositioning/Group-617.png"
-                    alt=""
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-              <div class="col my-1 socialDiv">
-                <a
-                  href="https://www.instagram.com/corporalityg"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="/img/BrandPositioning/Group-616.png"
-                    alt=""
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-              <div class="col my-1 socialDiv">
-                <a
-                  href="https://www.linkedin.com/company/corporality"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="/img/BrandPositioning/Group-615.png"
-                    alt=""
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-              <div class="col my-1 socialDiv">
-                <a
-                  href="https://in.pinterest.com/CorporalityG"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="/img/BrandPositioning/Group-614.png"
-                    alt=""
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
-              <div class="col my-1 socialDiv">
-                <a
-                  href="https://www.youtube.com/channel/UC4EISt8kHI4zzpmbIBMIBbg"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  <img
-                    src="/img/BrandPositioning/Group-613.png"
-                    alt=""
-                    className="img-fluid"
-                  />
-                </a>
-              </div>
+        <div className="social position-absolute end-0 top-50 translate-middle-y container">
+          <div class="row row-cols-1 mx-0 bpcp1SocialRow">
+            <div class="col my-1 socialDiv">
+              <a
+                href="https://www.facebook.com/CorporalityG"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/img/BrandPositioning/Group-618.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </a>
             </div>
+            <div class="col my-1 socialDiv">
+              <a
+                href="https://twitter.com/corporalityg"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/img/BrandPositioning/Group-617.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </a>
+            </div>
+            <div class="col my-1 socialDiv">
+              <a
+                href="https://www.instagram.com/corporalityg"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/img/BrandPositioning/Group-616.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </a>
+            </div>
+            <div class="col my-1 socialDiv">
+              <a
+                href="https://www.linkedin.com/company/corporality"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/img/BrandPositioning/Group-615.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </a>
+            </div>
+            <div class="col my-1 socialDiv">
+              <a
+                href="https://in.pinterest.com/CorporalityG"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/img/BrandPositioning/Group-614.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </a>
+            </div>
+            <div class="col my-1 socialDiv">
+              <a
+                href="https://www.youtube.com/channel/UC4EISt8kHI4zzpmbIBMIBbg"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img
+                  src="/img/BrandPositioning/Group-613.png"
+                  alt=""
+                  className="img-fluid"
+                />
+              </a>
+            </div>
+          </div>
         </div>
       </div>
       <div className="vw-100 my-2">
