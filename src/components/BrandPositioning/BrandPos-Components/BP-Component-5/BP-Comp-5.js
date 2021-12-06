5016382bb231df8eb55b5098b1406d5899dca225
Kunal <kunalmathur0803@gmail.com>
2021-12-06 13:51:37 +0530

    added responsivness to comp5

--- src/components/BrandPositioning/BrandPos-Components/BP-Component-5/BP-Comp-5.js
+++ src/components/BrandPositioning/BrandPos-Components/BP-Component-5/BP-Comp-5.js
@@ -7,7 +7,7 @@ function BPComponent5() {
   return (
     <div>
       <div className="BP-CP5-heading w-100">
-        <h1 className="BP-CP5-h1 w-100">
+        <h1 className="BP-CP5-h1  d-flex justify-content-center">
           Indomitable brand positioning strategies
         </h1>
       </div>
@@ -26,13 +26,13 @@ function BPComponent5() {
                 <Accordion.Header>
                   <div class="container">
                     <div class="row">
-                      <div class="col-4 col-md-3 col-lg-2">
+                      <div class="col-4 col-md-3 col-xl-2 bp-cp5-tm-div">
                         <img
                           src="../img/BrandPositioning/faviconTM1.png"
                           className="img-fluid img-fav"
                         />
                       </div>
-                      <div class="col-8 col-md-9 col-lg-10">
+                      <div class="col-8 col-md-9 col-xl-10">
                         <span className="bp-cp5-bodyspan">
                           Defining the true differentiators
                         </span>
@@ -43,13 +43,13 @@ function BPComponent5() {
                 <Accordion.Body>
                   <div class="container">
                     <div class="row">
-                      <div class="col-2">
+                      <div class="col-12 col-md-4 col-lg-3 col-xl-2">
                         <img
                           src="../img/BrandPositioning/Rectangle-1.png"
-                          className="img-fluid"
+                          className="img-fluid bp-cp5-listbodyimg"
                         />
                       </div>
-                      <div class="col-10  bp-cp5-listbody">
+                      <div class="col-12 col-md-8 col-lg-9 col-xl-10  bp-cp5-listbody">
                         Marketing and communication must be disposed of in a way
                         that will stir consumer interest so that you get noticed
                         by all. Therefore, we create a preference that will
@@ -66,13 +66,13 @@ function BPComponent5() {
                   <div class="container">
                     <div class="row">
                       <div class="row">
-                        <div class="col-4 col-md-3 col-lg-2">
+                        <div class="col-4 col-md-3 col-xl-2 bp-cp5-tm-div">
                           <img
                             src="../img/BrandPositioning/faviconTM2.png"
                             className="img-fluid img-fav"
                           />
                         </div>
-                        <div class="col-8 col-md-9 col-lg-10">
+                        <div class="col-8 col-md-9 col-xl-10">
                           <span className="bp-cp5-bodyspan">
                             Maintaining relevance
                           </span>
@@ -84,13 +84,13 @@ function BPComponent5() {
                 <Accordion.Body>
                   <div class="container">
                     <div class="row">
-                      <div class="col-2">
+                      <div class="col-12 col-md-4 col-xl-2">
                         <img
                           src="../img/BrandPositioning/Rectangle-2.png"
-                          className="img-fluid"
+                          className="img-fluid bp-cp5-listbodyimg"
                         />
                       </div>
-                      <div class="col-10 bp-cp5-listbody">
+                      <div class="col-12 col-md-8 col-xl-10  bp-cp5-listbody">
                         While true differentiators are important, compelling the
                         target audience is vital to the success of a brand. At
                         Corporality Global - the top digital marketing
@@ -108,13 +108,13 @@ function BPComponent5() {
                   <div class="container">
                     <div class="row">
                       <div class="row">
-                        <div class="col-4 col-md-3 col-lg-2">
+                        <div class="col-4 col-md-3 col-xl-2 bp-cp5-tm-div">
                           <img
                             src="../img/BrandPositioning/faviconTM3.png"
                             className="img-fluid img-fav"
                           />
                         </div>
-                        <div class="col-8 col-md-9 col-lg-10">
+                        <div class="col-8 col-md-9 col-xl-10">
                           <span className="bp-cp5-bodyspan">
                             Address and engage
                           </span>
@@ -126,13 +126,13 @@ function BPComponent5() {
                 <Accordion.Body>
                   <div class="container">
                     <div class="row">
-                      <div class="col-2">
+                      <div class="col-12 col-md-4 col-xl-2">
                         <img
                           src="../img/BrandPositioning/Rectangle-3.png"
-                          className="img-fluid "
+                          className="img-fluid bp-cp5-listbodyimg"
                         />
                       </div>
-                      <div class="col-10 bp-cp5-listbody">
+                      <div class="col-12 col-md-8 col-xl-10  bp-cp5-listbody">
                         Coherence is essential to engage with your audiences and
                         connect with them in meaningful ways. Our Sydney based
                         professional digital marketing create a unique value
@@ -148,13 +148,13 @@ function BPComponent5() {
                   <div class="container">
                     <div class="row">
                       <div class="row">
-                        <div class="col-4 col-md-3 col-lg-2">
+                        <div class="col-4 col-md-3 col-xl-2 bp-cp5-tm-div">
                           <img
                             src="../img/BrandPositioning/faviconTM4.png"
                             className="img-fluid img-fav"
                           />
                         </div>
-                        <div class="col-8 col-md-9 col-lg-10">
+                        <div class="col-8 col-md-9 col-xl-10">
                           <span className="bp-cp5-bodyspan">
                             Earn a reputation
                           </span>
@@ -166,13 +166,13 @@ function BPComponent5() {
                 <Accordion.Body>
                   <div class="container">
                     <div class="row">
-                      <div class="col-2">
+                      <div class="col-12 col-md-4 col-xl-2">
                         <img
                           src="../img/BrandPositioning/Rectangle-4.png"
-                          className="img-fluid"
+                          className="img-fluid bp-cp5-listbodyimg"
                         />
                       </div>
-                      <div class="col-10 bp-cp5-listbody">
+                      <div class="col-12 col-md-8 col-xl-10  bp-cp5-listbody">
                         Fulfilling expectations over and over again gives you a
                         reputation of being a committed performer. We add
                         intricate innovations to your brand positioning strategy
