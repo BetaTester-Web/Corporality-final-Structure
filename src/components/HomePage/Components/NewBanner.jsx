616d781d6e517215492d00dfa3947d1e0caa794e
Nikit <singhnikit16@gmail.com>
2021-12-12 07:40:20 +0530

    homepage banner re-aligned

--- src/components/HomePage/Components/NewBanner.jsx
+++ src/components/HomePage/Components/NewBanner.jsx
@@ -25,61 +25,64 @@ const NewBanner = () => {
     return (
         <div className="bannerContainer">
             <video className="bannerVideo" src="/img/HomePage/media/banner-vid-latest.mp4" type="video/mp4" loop autoPlay muted></video>
-            <div className='maxContainer'>
-            <div className="leftBannerContainer">
-                <div className="leftIndicators">
-                    {/* <div>{indicatorId}</div> */}
-                    <div className={`indicatorCircle ${indicatorId === 1? "activeIndicatorCircle" : ""}`} onClick={() => setIndicatorId(1)} ></div>
-                    <div className={`indicatorCircle ${indicatorId === 2? "activeIndicatorCircle" : ""}`} onClick={() => setIndicatorId(2)}></div>
-                    <div className={`indicatorCircle ${indicatorId === 3? "activeIndicatorCircle" : ""}`} onClick={() => setIndicatorId(3)}></div>
-                </div>
-                <div className="rightPartBanner">
-                    <div data-aos="fade-right" className={`bannerText ${indicatorId===1? "" : "bannerTextHide"}`}>
-                        <h1 className="bannerHead">global sales and marketing company</h1>
-                        <p className="bannerSubtitle">
-                            Identifying every opportunity to convert your potentials into ready-to-buy states.
-                        </p>
-                        <p className="bannerSubtitle">
-                            Leading Management Consulting Firm Australia.
-                        </p>
+            <div className='leftBannerContainerWrapper'>
+                <div className="leftBannerContainer">
+                    <div className="leftIndicators">
+                        {/* <div>{indicatorId}</div> */}
+                        <div className={`indicatorCircle ${indicatorId === 1? "activeIndicatorCircle" : ""}`} onClick={() => setIndicatorId(1)} ></div>
+                        <div className={`indicatorCircle ${indicatorId === 2? "activeIndicatorCircle" : ""}`} onClick={() => setIndicatorId(2)}></div>
+                        <div className={`indicatorCircle ${indicatorId === 3? "activeIndicatorCircle" : ""}`} onClick={() => setIndicatorId(3)}></div>
                     </div>
-                    <div className={`bannerText ${indicatorId===2? "" : "bannerTextHide"}`}>
-                        <h1 className="bannerHead">Reimagining your boundaries</h1>
-                        <p className="bannerSubtitle">
-                            Expanding your reach to provide more than just marketing solutions
-                            Superior Online Marketing Company Australia
-                        </p>
-                    </div>
-                    <div className={`bannerText ${indicatorId===3? "" : "bannerTextHide"}`}>
-                        <h1 className="bannerHead">The future of businesses is right here</h1>
-                        <p className="bannerSubtitle">
-                            Implement the most effective digital marketing strategies for your business
-                            Premier marketing company Australia
-                        </p>
-                    </div>
-                    <div className="bannerSocialLinksContainer">
-                        <h2 className="bannerSocialText">Social</h2>
-                        <div className="socialIconsContainer">
-                            <div className="socialIcon" onClick={() => window.open("https://www.facebook.com/CorporalityG/")}>
-                                <i className="fa fa-facebook-f"></i>
-                            </div>
-                            <div className="socialIcon" onClick={() => window.open("https://twitter.com/corporalityg/")}>
-                                <i className="fa fa-twitter"></i>
-                            </div>
-                            <div className="socialIcon" onClick={() => window.open("https://www.instagram.com/corporalityg/")}>
-                                <i className="fa fa-instagram"></i>
-                            </div>
-                            <div className="socialIcon" onClick={() => window.open("https://www.linkedin.com/company/corporalityg/")}>
-                                <i className="fa fa-linkedin"></i>
-                            </div>
-                            <div className="socialIcon" onClick={() => window.open("https://in.pinterest.com/CorporalityG/_created/")}>
-                                <i className="fa fa-pinterest"></i>
-                            </div>
-                            <div className="socialIcon" onClick={() => window.open("https://www.youtube.com/channel/UC4EISt8kHI4zzpmbIBMIBbg")}>
-                                <i className="fa fa-youtube-play"></i>
+                    <div className="rightPartBanner">
+                        <div data-aos="fade-right" className={`bannerText ${indicatorId===1? "" : "bannerTextHide"}`}>
+                            <h1 className="bannerHead">global sales and marketing company</h1>
+                            <p className="bannerSubtitle">
+                                Identifying every opportunity to convert your potentials into ready-to-buy states.
+                            </p>
+                            <p className="bannerSubtitle">
+                                Leading Management Consulting Firm Australia.
+                            </p>
+                        </div>
+                        <div className={`bannerText ${indicatorId===2? "" : "bannerTextHide"}`}>
+                            <h1 className="bannerHead">Reimagining your boundaries</h1>
+                            <p className="bannerSubtitle">
+                                Expanding your reach to provide more than just marketing solutions
+                                Superior Online Marketing Company Australia
+                            </p>
+                        </div>
+                        <div className={`bannerText ${indicatorId===3? "" : "bannerTextHide"}`}>
+                            <h1 className="bannerHead">The future of businesses is right here</h1>
+                            <p className="bannerSubtitle">
+                                Implement the most effective digital marketing strategies for your business
+                                Premier marketing company Australia
+                            </p>
+                        </div>
+                        <div className="bannerSocialLinksContainer">
+                            <h2 className="bannerSocialText">Social</h2>
+                            <div className="socialIconsContainer">
+                                <div className="socialIcon" onClick={() => window.open("https://www.facebook.com/CorporalityG/")}>
+                                    <i className="fa fa-facebook-f"></i>
+                                </div>
+                                <div className="socialIcon" onClick={() => window.open("https://twitter.com/corporalityg/")}>
+                                    <i className="fa fa-twitter"></i>
+                                </div>
+                                <div className="socialIcon" onClick={() => window.open("https://www.instagram.com/corporalityg/")}>
+                                    <i className="fa fa-instagram"></i>
+                                </div>
+                                <div className="socialIcon" onClick={() => window.open("https://www.linkedin.com/company/corporalityg/")}>
+                                    <i className="fa fa-linkedin"></i>
+                                </div>
+                                <div className="socialIcon" onClick={() => window.open("https://in.pinterest.com/CorporalityG/_created/")}>
+                                    <i className="fa fa-pinterest"></i>
+                                </div>
+                                <div className="socialIcon" onClick={() => window.open("https://www.youtube.com/channel/UC4EISt8kHI4zzpmbIBMIBbg")}>
+                                    <i className="fa fa-youtube-play"></i>
+                                </div>
                             </div>
                         </div>
                     </div>
+                    <img src="/img/HomePage/media/random-square.png" alt="" className="square-1" />
+                    <img src="/img/HomePage/media/random-square-1.png" alt="" className={`square-2 ${indicatorId===1 ? "": "square-2-diff"}`} />
                 </div>
             </div>
             <div className="rightBannerContainer"  onClick={rightBannerClickHandler}>
@@ -98,9 +101,7 @@ const NewBanner = () => {
                     <img src="/img/HomePage/media/smallRectangle.png" alt="" />
                 </div>
             </div>
-            <img src="/img/HomePage/media/random-square.png" alt="" className="square-1" />
-            <img src="/img/HomePage/media/random-square-1.png" alt="" className={`square-2 ${indicatorId===1 ? "": "square-2-diff"}`} />
-            </div>
+            {/* </div> */}
         </div>
     )
 }
