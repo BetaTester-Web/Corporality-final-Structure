8f47d2c1d71a1fe37c3e3b8603f013ba7ace7753
Nikit <singhnikit16@gmail.com>
2021-12-18 01:13:37 +0530

    blogtop container css fixed

--- src/components/Blog/components/BlogTop.js
+++ src/components/Blog/components/BlogTop.js
@@ -5,9 +5,22 @@ import blogTopLeft from "../img/blog-top-left.png"
 import searchIcon from "../img/search-icon.png"
 import DropDown from "./DropDown";
 
-function BlogTop() {
-    const htmlString = "<p><strong>It is what it is.</strong><br />\
-    Hehe just kidding..</p><p><s><em>He&#39;s god.</em></s></p>"
+function BlogTop({blogState}) {
+    const {searchArticles, setSearchArticles, articles} = blogState;
+    console.log(articles);
+    const handleBlogSearch = (e) => {
+        if(e.target.value.length > 0) {
+            let articlesList = articles.filter(article => {
+                let regex = new RegExp(e.target.value, "gi");
+                return article.title.match(regex) || article.description.match(regex);
+            })
+            setSearchArticles(articlesList);
+        }else{
+            setSearchArticles(articles);
+        }
+    }
+
+
     return (
         <div className="blogTopContainer">
             <div className="blogSubscription">
@@ -22,17 +35,19 @@ function BlogTop() {
                 </select>
                 <div className="subscribeButton">Subscribe</div>
             </div>
-            <div className="blogMarket">
-                <img src={blogTopLeft} alt="" />
-                <div>
-                    <h3>Do you want me to do your marketing for you?</h3>
-                    <div>Yes, I want</div>
+            <div className="blogMarketWrapper">
+                <div className="blogMarket">
+                    <img src={blogTopLeft} alt="" />
+                    <div>
+                        <h3>Do you want me to do your marketing for you?</h3>
+                        <div>Yes, I want</div>
+                    </div>
                 </div>
             </div>
             <div className="blogCustomizer">
-                <DropDown className="blogDropDown" initial="Recent Post" list={['Recent Post']} />
+                <DropDown className="blogDropDown" initial="Recent Post" list={[...searchArticles]} />
                 <div className="blogSearchInput">
-                    <input type="text" placeholder="Search" />
+                    <input type="text" placeholder="Search" onChange={(e) => handleBlogSearch(e)} />
                     <img src={searchIcon} alt="" />
                 </div>
                 <DropDown className="blogDropDown" initial="Select Category" list={['Corporate Strategy','Digital Media Marketing', 'Lead Gen and Sales Strategy', 'Marketing Strategy for 2021', 'Product Strategy', 'Professional Practices Strategy', 'Website Blueprint']} />
--- src/components/Blog/components/DropDown.jsx
+++ src/components/Blog/components/DropDown.jsx
@@ -1,21 +1,37 @@
 import React, { useState } from 'react'
+import { Link, useNavigate } from 'react-router-dom'
 import "./DropDown.css"
 
 const DropDown = ({initial, list, className}) => {
     if(!list){
         list = []
     }
+    const navigate = useNavigate();
+    console.log(list)
     const [select, setSelect] = useState(initial || '')
     return (
         <div className={`dropdown customDropDown ${className || ''}`}>
             <button className="btn dropdown-toggle" type="button" id="dropdownMenuButton2" data-bs-toggle="dropdown" aria-expanded="false">
                 {select}
             </button>
-            <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton2">
-                {
-                    list.map((item, index) => <li key={index} onClick={() => setSelect(item)}><a className="dropdown-item" href="#">{item}</a></li>)
-                }
-            </ul>
+            {
+                initial !== "Recent Post"?
+                <ul className={`dropdown-menu${initial=="Recent Post"? " recentPostContainer" : ""}`} aria-labelledby="dropdownMenuButton2">
+                    {
+                        list.map((item, index) => <li key={index} onClick={() => setSelect(item)}><a className="dropdown-item" href="#">{item}</a></li>)
+                    }
+                </ul>
+                :
+                <ul className={`dropdown-menu${initial=="Recent Post"? " recentPostContainer" : ""}`} aria-labelledby="dropdownMenuButton2">
+                    {
+                        list.map((item, index) => (
+                        <li key={index} >
+                            <img src={item.photo} alt="" />
+                            <Link to={`/${item.slug}`} className="dropdown-item">{item.title}</Link>
+                        </li>))
+                    }
+                </ul>
+            }
     </div>
     )
 }
