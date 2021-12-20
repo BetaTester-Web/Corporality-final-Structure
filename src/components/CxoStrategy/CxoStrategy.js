d30b47dd174c2c835a95163a168a4592b3f85739
Nikit <singhnikit16@gmail.com>
2021-12-21 01:55:19 +0530

    CXO charge and step comp done

--- src/components/CxoStrategy/CxoStrategy.js
+++ src/components/CxoStrategy/CxoStrategy.js
@@ -1,19 +1,18 @@
 import React from "react";
-
-// import Talent from "./Components/Talent";
-import Charge from "./Components/Charge";
-import Step from "./Components/Step";
+import Talent from "./Components/Talent";
 import Continously from "./Components/Continously";
 import Leverage from "./Components/Leverage";
 import Differentiate from "./Components/Differentiate";
+import NewCharge from "./Components/NewCharge";
+import NewStep from "./Components/NewStep";
 
 function HomePage() {
   return (
     <>
       <Differentiate />
-      <Charge />
-      <Step />
-      {/* <Talent /> */}
+      <NewCharge />
+      <NewStep />
+      <Talent />
       <Continously />
       <Leverage />
     </>
