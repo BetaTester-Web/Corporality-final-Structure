a767bb97a289028ee37a7fd7e1ce275fb0857b7d
MVRKKSK <miryala.kautilya2020@vitbhopal.ac.in>
2021-12-20 18:58:29 +0530

    completed all errors

--- src/components/SustainableGrowth/components/GlobalAIms/Global.jsx
+++ src/components/SustainableGrowth/components/GlobalAIms/Global.jsx
@@ -15,23 +15,23 @@ const Global = () => {
     <div>
       <div className="container container-Global d-flex justify-content-center flex-column align-items-center">
         <div className="row image-under-text">
-          <div className="Global-text-1">
+          <div data-aos = "fade-down" data-aos-delay = "500"  className="Global-text-1">
             "By 2025, Corporality Global aims to become an epitome of
             sustainability, worldwide."
           </div>
           
         </div>
         <div className="row">
-          <p className="Global-text-2">Priya Mishra</p>
+          <p data-aos = "fade-down" data-aos-delay = "1000" className="Global-text-2">Priya Mishra</p>
         </div>
         <div className="row">
-          <p className="Global-text-3">CEO, Corporality Global</p>
+          <p data-aos = "fade-down" data-aos-delay = "1000" className="Global-text-3">CEO, Corporality Global</p>
         </div>
         <div className="row d-flex justify-content-center  align-items-center">
           <div className="col">
             <a href="https://twitter.com/corporalityg/" className="global_link">
-              <div className="social_icons_Sustainable_growth d-flex justify-content-center align-items-center">
-                <div className="global_twitter_circle">
+              <div data-aos = "fade-right" data-aos-delay="1500" className="social_icons_Sustainable_growth d-flex justify-content-center align-items-center">
+                <div  className="global_twitter_circle">
                   <img
                     src={circle}
                     alt=" "
@@ -57,8 +57,8 @@ const Global = () => {
             </a>
           </div>
           <div className="col">
-            <a className="global_link" href="https://www.linkedin.com/company/corporalityg/">
-              <div className="social_icons2_Sustainable_growth d-flex justify-content-center align-items-center">
+            <a  className="global_link" href="https://www.linkedin.com/company/corporalityg/">
+              <div data-aos = "fade-left" data-aos-delay = "2000"  className="social_icons2_Sustainable_growth d-flex justify-content-center align-items-center">
                 <div className="global_twitter_circle">
                   <img
                     src={circle}
