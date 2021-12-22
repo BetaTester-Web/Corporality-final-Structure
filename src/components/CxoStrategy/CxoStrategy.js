ada885ea1de9b7a1dbbbddafccb3839ead9f64ff
Nikit <singhnikit16@gmail.com>
2021-12-22 05:44:30 +0530

    CXO comp 1 done

--- src/components/CxoStrategy/CxoStrategy.js
+++ src/components/CxoStrategy/CxoStrategy.js
@@ -5,16 +5,16 @@ import Leverage from "./Components/Leverage";
 import Differentiate from "./Components/Differentiate";
 import NewCharge from "./Components/NewCharge";
 import NewStep from "./Components/NewStep";
-
 import NewLeverage from "./Components/NewLeverage";
 import NewContinously from "./Components/NewContinously";
-
 import NewTalent from "./Components/NewTalent";
+import NewDifferentiate from "./Components/NewDifferentiate";
 
 
 function HomePage() {
   return (
     <>
+      <NewDifferentiate />
       {/* <Differentiate /> */}
       <NewCharge />
       <NewStep />
